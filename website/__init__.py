1330d2c1154040a08e325a7f678f771ff939c8bf
Kaue Reinbold <kaue.reinbold@outlook.com>
2021-10-13 09:24:29 -0300

    feat: regsiter home route

--- website/__init__.py
+++ website/__init__.py
@@ -3,7 +3,12 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+
     # HACK: Add the key into a .env file
     app.config['SECRET_KEY'] = '64c1d708-3537-428f-9807-d57df3704215'
+
+    from .views import views
+
+    app.register_blueprint(views, url_prefix="/")
 
     return app
