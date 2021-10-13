16ab7fba0ac13bc8f8dddf59065944df4d256aeb
Kaue Reinbold <kaue.reinbold@outlook.com>
2021-10-13 09:17:44 -0300

    feat: add web server configuration

---
+++ website/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+    # HACK: Add the key into a .env file
+    app.config['SECRET_KEY'] = '64c1d708-3537-428f-9807-d57df3704215'
+
+    return app
