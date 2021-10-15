bad46197f0bc771658b1d641300b2099c334175c
Kaue Reinbold <kaue.reinbold@outlook.com>
2021-10-15 09:42:02 -0300

    feat: add note for the user

--- website/views.py
+++ website/views.py
@@ -1,10 +1,27 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from flask.helpers import flash
 from flask_login import login_required, current_user
+
+from .models import Note
+from . import db
 
 views = Blueprint('views', __name__)
 
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
+    if request.method == 'POST':
+        note_text = request.form.get('note')
+
+        if len(note_text) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            note = Note(text=note_text, user_id=current_user.id)
+
+            db.session.add(note)
+            db.session.commit()
+
+            flash('Note added!', category='success')
+
     return render_template('home.html', user=current_user)
