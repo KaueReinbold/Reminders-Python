51b34892bde3c302c583c0c37b8cdaa2aeabfd47
Kaue Reinbold <kaue.reinbold@outlook.com>
2021-10-15 09:30:32 -0300

    feat? control access for user logged in

--- website/auth.py
+++ website/auth.py
@@ -26,7 +26,7 @@ def login():
         else:
             flash('Email or password not valid!', category='error')
 
-    return render_template('login.html')
+    return render_template('login.html', user=current_user)
 
 
 @auth.route('logout')
@@ -73,4 +73,4 @@ def sign_up():
 
             return redirect(url_for('views.home'))
 
-    return render_template('sign_up.html')
+    return render_template('sign_up.html', user=current_user)
