bfabc05217701e5c211205c4d7d1199572d53b06
Kaue Reinbold <kaue.reinbold@outlook.com>
2021-10-17 11:59:25 -0300

    refactor: remove no used function

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from website import create_app
 from src.data import setup_data
 
-from src.service import setup_service
-
 app = create_app()
 
 setup_data(app)
-setup_service()
 
 if __name__ == '__main__':
     # HACK: Get environment from the .env file
